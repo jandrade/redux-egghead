0771bc9d8da4a621b6a3cdb460f82d353724fe2a
Juan Andrade <juandavidandrade@gmail.com>
2017-02-03 13:38:35 -0500

    test(counter): Refactor counter tests

--- test/counter.test.js
+++ test/counter.test.js
@@ -1,15 +1,5 @@
 import expect from 'expect';
-
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
-}
+import counter from '../src/counter.js';
 
 expect(
     counter(0, {type: 'INCREMENT'})
