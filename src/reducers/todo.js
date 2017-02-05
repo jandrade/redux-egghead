4ab0110f314f320ca125df357643e2c12c38a5ab
Juan Andrade <juandavidandrade@gmail.com>
2017-02-05 11:40:04 -0500

    feat(todo): Add reducer to toggle a todo

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -12,6 +12,20 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ]
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id !== action.id) {
+                    return todo;
+                }
+
+                /*return {
+                    ...todo,
+                    completed: !todo.completed
+                };*/
+                return Object.assign({}, todo, {
+                    completed: !todo.completed
+                });
+            });
         default:
             return state;
     }
@@ -40,5 +54,44 @@ const testAddTodo = () => {
     ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Learn react',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Learn react',
+            completed: true
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed!');
