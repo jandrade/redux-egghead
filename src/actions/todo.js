59aecb8d5df35785d070c8044346b13389eabae6
Juan Andrade <juandavidandrade@gmail.com>
2017-02-14 12:29:08 -0500

    feat(state): Persisting the state to the Local Storage

--- src/actions/todo.js
+++ src/actions/todo.js
@@ -1,16 +1,23 @@
-/**
- * @type {string}
- * Todo Identifier
- */
-let nextTodoId = 0;
+import { v4 } from 'node-uuid';
 
+/**
+ * Creates a new todo
+ * 
+ * @param {String} text
+ * @return {Object} Created todo
+ */
 export const addTodo = (text) => ({
     type: 'ADD_TODO',
     text: text,
-    id: (nextTodoId++).toString()
+    id: v4()
 });
 
-
+/**
+ * Marks a todo as completed/active
+ * 
+ * @param {String} id
+ * @return {Object} Toggled todo
+ */
 export const toggleTodo = (id) => ({
     type: 'TOGGLE_TODO',
     id
--- src/todoApp.js
+++ src/todoApp.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { connect, Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import throttle from 'lodash/throttle';
+
 // components
 import AddTodo from './components/addTodo';
 import VisibleTodoList from './components/todoList';
 import Footer from './components/footer';
 // reducers
 import rootReducer from './reducers/index';
+
+import { loadState, saveState } from './helpers/localStorage';
 
 /**
  * Main Application
@@ -24,20 +28,18 @@ const TodoApp = () => (
     </div>
 );
 
-const persistedState = {
-    todos: [
-        {
-            id: 0,
-            text: 'Welcome back!',
-            completed: false
-        }
-    ]
-};
+const persistedState = loadState();
 
 const store = createStore(
     rootReducer,
     persistedState
 );
+
+store.subscribe(throttle(() => {
+    saveState({
+        todos: store.getState().todos
+    });
+}, 1000));
 
 const render = () => {
     ReactDOM.render(
